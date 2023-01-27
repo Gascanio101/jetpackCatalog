8255114979707db4be6dda89998d2ab61f382dc0
Gabriel Ascanio <80049275+Gascanio101@users.noreply.github.com>
2023-01-27 11:51:00 +0100

    Added button with counter on tap, then shows the count in a Text composable updated in real time

--- jetpackCatalog/app/src/main/java/com/example/jetpaccomposecatalog/MainActivity.kt
+++ jetpackCatalog/app/src/main/java/com/example/jetpaccomposecatalog/MainActivity.kt
@@ -7,10 +7,13 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -30,7 +33,9 @@ class MainActivity : ComponentActivity() {
                 ) {
 //                    MyBox(name = "Android")
 //                    MyColumn()
-                    MyRow()
+//                    MyRow()
+//                    MyComplexLayout()
+                    MyStateExample()
                 }
             }
         }
@@ -39,162 +44,235 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyColumn() {
-    Column(modifier = Modifier
-        .fillMaxSize()
-        .verticalScroll(rememberScrollState())
-        ,verticalArrangement = Arrangement.SpaceAround ) {
-        Text("Example 01", modifier = Modifier
-            .background(Color.Cyan)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.Cyan)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.DarkGray)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.Red)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.Green)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.Magenta)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.Yellow)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.Cyan)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.DarkGray)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.Red)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.Green)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.Magenta)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.Yellow)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.Cyan)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.DarkGray)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.Red)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.Green)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.Magenta)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.Yellow)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.Cyan)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.DarkGray)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.Red)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.Green)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.Magenta)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.Yellow)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.Cyan)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.DarkGray)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.Red)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.Green)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.Magenta)
-            .fillMaxWidth()
-            .height(50.dp))
-        Text("Example 01", modifier = Modifier
-            .background(Color.Yellow)
-            .fillMaxWidth()
-            .height(50.dp))
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState()), verticalArrangement = Arrangement.SpaceAround
+    ) {
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.Cyan)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.Cyan)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.DarkGray)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.Red)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.Green)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.Magenta)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.Yellow)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.Cyan)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.DarkGray)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.Red)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.Green)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.Magenta)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.Yellow)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.Cyan)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.DarkGray)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.Red)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.Green)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.Magenta)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.Yellow)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.Cyan)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.DarkGray)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.Red)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.Green)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.Magenta)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.Yellow)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.Cyan)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.DarkGray)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.Red)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.Green)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.Magenta)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
+        Text(
+            "Example 01", modifier = Modifier
+                .background(Color.Yellow)
+                .fillMaxWidth()
+                .height(50.dp)
+        )
     }
 }
 
 @Composable
 fun MyRow() {
-    Row(modifier = Modifier
-        .background(Color.Yellow)
-        .fillMaxWidth()
-        .height(120.dp), horizontalArrangement = Arrangement.SpaceAround) {
-        Box(modifier = Modifier
-            .fillMaxHeight()
-            .width(240.dp)
-            .background(Color.DarkGray),
-            contentAlignment = Alignment.Center) {
-            Row(modifier = Modifier
-                .fillMaxSize()
-                .padding(10.dp), horizontalArrangement = Arrangement.SpaceAround) {
+    Row(
+        modifier = Modifier
+            .background(Color.Yellow)
+            .fillMaxWidth()
+            .height(120.dp), horizontalArrangement = Arrangement.SpaceAround
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxHeight()
+                .width(240.dp)
+                .background(Color.DarkGray),
+            contentAlignment = Alignment.Center
+        ) {
+            Row(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(10.dp), horizontalArrangement = Arrangement.SpaceAround
+            ) {
 
-                Box(modifier = Modifier
-                    .width(100.dp)
-                    .height(100.dp)
-                    .background(Color.Cyan), contentAlignment = Alignment.Center) {
+                Box(
+                    modifier = Modifier
+                        .width(100.dp)
+                        .height(100.dp)
+                        .background(Color.Cyan), contentAlignment = Alignment.Center
+                ) {
                     Text("Left Box")
                 }
-                Box(modifier = Modifier
-                    .width(100.dp)
-                    .height(100.dp)
-                    .background(Color.Cyan), contentAlignment = Alignment.Center) {
+                Box(
+                    modifier = Modifier
+                        .width(100.dp)
+                        .height(100.dp)
+                        .background(Color.Cyan), contentAlignment = Alignment.Center
+                ) {
                     Text("Right Box")
                 }
             }
@@ -204,37 +282,81 @@ fun MyRow() {
 
 @Composable
 fun MyBox(name: String) {
-    Box(modifier = Modifier
-        .fillMaxSize()
-        .background(Color.Blue), contentAlignment = Alignment.Center) {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Blue), contentAlignment = Alignment.Center
+    ) {
         // Box(modifier = Modifier.width(50.dp).height(50.dp).background(Color.Cyan))
-        Box(modifier = Modifier
-            .height(200.dp)
-            .width(200.dp)
-            .background(Color.Cyan)
-            .verticalScroll(
-                rememberScrollState()
-            ), contentAlignment = Alignment.Center) {
+        Box(
+            modifier = Modifier
+                .height(200.dp)
+                .width(200.dp)
+                .background(Color.Cyan)
+                .verticalScroll(
+                    rememberScrollState()
+                ), contentAlignment = Alignment.Center
+        ) {
             Text("Fullfill my needs")
         }
+    }
+}
+
+@Preview
+@Composable
+fun MyStateExample() {
+    var counter = remember { mutableStateOf(0) }
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Button(onClick = { counter.value += 1 }) {
+            Text("Pulsame")
+        }
+        Text("He sido pulsado ${counter.value} veces")
     }
 }
 
 @Composable
 fun MyComplexLayout() {
     Column(modifier = Modifier.fillMaxSize()) {
-        Box(modifier = Modifier.background(Color.Cyan).fillMaxWidth().weight(1f), contentAlignment = Alignment.Center) {
+        Box(
+            modifier = Modifier
+                .background(Color.Cyan)
+                .fillMaxWidth()
+                .weight(1f), contentAlignment = Alignment.Center
+        ) {
             Text("Example 1")
         }
-        Row(modifier = Modifier.fillMaxWidth().weight(1f)) {
-            Box(modifier = Modifier.background(Color.Red).fillMaxHeight().weight(1f), contentAlignment = Alignment.Center) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
+        ) {
+            Box(
+                modifier = Modifier
+                    .background(Color.Red)
+                    .fillMaxHeight()
+                    .weight(1f), contentAlignment = Alignment.Center
+            ) {
                 Text("Example 2")
             }
-            Box(modifier = Modifier.background(Color.Green).fillMaxHeight().weight(1f), contentAlignment = Alignment.Center) {
+            Box(
+                modifier = Modifier
+                    .background(Color.Green)
+                    .fillMaxHeight()
+                    .weight(1f), contentAlignment = Alignment.Center
+            ) {
                 Text("Example 3")
             }
         }
-        Box(modifier = Modifier.background(Color.Magenta).fillMaxWidth().weight(1f), contentAlignment = Alignment.BottomCenter) {
+        Box(
+            modifier = Modifier
+                .background(Color.Magenta)
+                .fillMaxWidth()
+                .weight(1f), contentAlignment = Alignment.BottomCenter
+        ) {
             Text("Example 4")
         }
     }
@@ -245,7 +367,8 @@ fun MyComplexLayout() {
 @Composable
 fun DefaultPreview() {
     JetpacComposeCatalogTheme {
-        MyComplexLayout()
+        MyStateExample()
+//        MyComplexLayout()
 //        MyRow()
 //        MyBox("Android")
 //        MyColumn()
