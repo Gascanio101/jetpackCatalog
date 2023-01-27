6fe008c10cd552f068236eb14b9f15a98f226692
Gabriel Ascanio <80049275+Gascanio101@users.noreply.github.com>
2023-01-27 14:53:05 +0100

    'Text' component basic knowledge

--- JetpackComponents/app/src/main/java/com/example/jetpackcomponentcatalog/MainActivity.kt
+++ JetpackComponents/app/src/main/java/com/example/jetpackcomponentcatalog/MainActivity.kt
@@ -1,15 +1,26 @@
 package com.example.jetpackcomponentcatalog
 
+import android.graphics.Paint.Style
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.example.jetpackcomponentcatalog.ui.theme.JetpackComponentCatalogTheme
 
 class MainActivity : ComponentActivity() {
@@ -22,22 +33,49 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MyText()
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JetpackComponentCatalogTheme {
-        Greeting("Android")
+        MyText()
     }
+}
+
+
+@Composable
+fun MyText() {
+    Column(
+        Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text("Hola Gabs")
+        Text("Hola Gabs", color = Color.Blue)
+        Text("Hola Gabs", fontWeight = FontWeight.ExtraBold)
+        Text("Hola Gabs", fontWeight = FontWeight.Light)
+        Text("Hola Gabs", fontFamily = FontFamily.Cursive)
+        Text("Hola Gabs", style = TextStyle(textDecoration = TextDecoration.Underline))
+        Text("Hola Gabs", style = TextStyle(textDecoration = TextDecoration.LineThrough))
+        Text(
+            "Hola Gabs", style = TextStyle(
+                textDecoration = TextDecoration.combine(
+                    listOf(TextDecoration.LineThrough, TextDecoration.Underline)
+                )
+            )
+        )
+        Text(
+            "Hola Gabs", textDecoration = TextDecoration.combine(
+                listOf(TextDecoration.LineThrough, TextDecoration.Underline)
+            ), fontSize = 30.sp, fontFamily = FontFamily.Cursive
+        )
+    }
+
 }
